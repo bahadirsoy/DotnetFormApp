cf324116e2d8f2cd851a9650b8194333111554b2
Bahadir Ustabasi <oxikairos@hotmail.com>
2021-07-25 12:33:56 +0300

    mvc added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetFormApp.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
